2f76e905e152a82e5b42c355800888480e4fc29d
BahirathyR <rbahirathy@gmail.com>
2021-11-16 22:53:17 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-
-function bigsumofarray(arry)
 var tot=0;
+
+function bigsumofarray(arry){
 for(let i=0;i<arry.length;i++){
     tot +=arry[i]
 }
 console.log(tot)
+}
 
 
 module.exports.bigsumofarray=bigsumofarray
